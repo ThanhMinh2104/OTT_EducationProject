4306f7b5f9d10258d3a239ecbcb9f8d38f4a9a18
taitai3 <tai230304@gmail.com>
2026-06-03 03:12:02 +0700

    fix: mobile build - remove barcode scanner, fix call events, add cleartext traffic

--- mobile/src/utils/config.ts
+++ mobile/src/utils/config.ts
@@ -1,3 +1,7 @@
 
 export const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://192.168.1.7:5000";
 
+// Debug: log URL đang dùng
+console.log('🌐 API_URL =', API_URL);
+console.log('🌐 EXPO_PUBLIC_API_URL env =', process.env.EXPO_PUBLIC_API_URL);
+
