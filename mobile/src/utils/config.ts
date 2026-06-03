15d45c5d44f512487bda1e6b26c9a257763f5b69
taitai3 <tai230304@gmail.com>
2026-06-03 09:50:43 +0700

    fix: update API_URL fallback to production server 54-179-135-20.nip.io

--- mobile/src/utils/config.ts
+++ mobile/src/utils/config.ts
@@ -1,7 +1,6 @@
 
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://192.168.1.7:5000";
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://54-179-135-20.nip.io";
 
 // Debug: log URL đang dùng
 console.log('🌐 API_URL =', API_URL);
 console.log('🌐 EXPO_PUBLIC_API_URL env =', process.env.EXPO_PUBLIC_API_URL);
-
